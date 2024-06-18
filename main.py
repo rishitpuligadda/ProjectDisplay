9cbb9e5dd7aa3b1f2738d8c53c9f09283b791e08
Rishit <puligaddarishit@gmail.com>
2024-06-18 07:36:02 +0530

    Created columns and my description

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images\\download.jpg", width=400)
+
+with col2:
+    st.title("Rishit")
+    content = """I really hope I have something
+    to write here in the future.
+    For now let's leave it as it is"""
+    st.info(content)
+
+st.write("Below you can find some of the apps I have built in python. Would be building many more in the future ;)")
+
+col3, col4 = st.columns(2)
