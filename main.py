448e9d4dbdc7d2031322f769ef2142370dd6821e
Rishit <puligaddarishit@gmail.com>
2024-06-18 07:49:08 +0530

    Add the titles

--- main.py
+++ main.py
@@ -17,4 +17,14 @@ with col2:
 
 st.write("Below you can find some of the apps I have built in python. Would be building many more in the future ;)")
 
+df = pandas.read_csv("data.csv", sep=";")
+
 col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
